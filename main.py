0ead89c0565a0661645b6f06500ccfa083577f1f
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-01-23 23:25:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import time
 import json
 import flask
 from flask import Flask, request, Response
@@ -10,6 +9,7 @@ import telebot
 app = Flask(__name__)
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 results_main = []
+main_dict = {}
 
 @app.route('/', methods=['POST', 'GET'])
 def handle_request():
@@ -29,29 +29,23 @@ def handle_request():
         return Response("OK", status=200)
     else:
         return ""
+    
+    
+@bot.message_handler(commands=["renew"])
+def randomMJRecent(m):
+    main_dict[m.chat.id] = results_main
+    
+    
+@bot.message_handler(commands=["next"])
+def sendNewImage(m):
+    image = main_dict[m.chat.id].pop()
+    bot.send_photo(
+        chat_id=m.chat.id,
+        photo=image["link"],
+        caption=image["prompt"]
+    )
 
-@bot.message_handler(commands=["new"])
-def new(m):
-    bot.send_message(chat_id="652015662", text="Hello from main")
-    results_main.clear()
-    response = requests.get("https://www.midjourney.com/showcase/recent/")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    scripts = soup.find_all("script")
-    for script in scripts:
-        try:
-            bot.send_message(chat_id="652015662", text=f"Sending {script['id'][7:-2].lower()}...")
-            data = json.loads(script.text)
-            jobs = data['props']['pageProps']['jobs']
-            for job in jobs:
-                results_main.append({"link": job['image_paths'][0],
-                                     "prompt": job['full_command']
-                                     })
-
-        except KeyError:
-            continue
-
-    bot.send_photo(chat_id="652015662", photo=results_main[0]['link'], caption=results_main[0]['prompt'])
-
+    
 def some_func():
     bot.send_message(chat_id="652015662", text="Hello from some_func")
     results_main.clear()
@@ -71,7 +65,6 @@ def some_func():
         except KeyError:
             continue
 
-    bot.send_photo(chat_id="652015662", photo=results_main[0]['link'], caption=results_main[0]['prompt'])
     
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
 
