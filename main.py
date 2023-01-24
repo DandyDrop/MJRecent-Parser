36d207523e6ea7b1a3e28ea1f28ad421d8774f96
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-01-24 12:34:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ from bs4 import BeautifulSoup
 import requests
 import telebot
 
-# bot = telebot.TeleBot(os.environ.get("TOKEN"))
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 app = Flask(__name__)
-# results_main = []
-# main_dict = {}
+results_main = []
+main_dict = {}
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -34,5 +33,25 @@ def randomMJRecent(m):
     bot.send_message(m.chat.id, "Anus")
 
 
+def main():
+    bot.send_message(chat_id="652015662", text="Executed the main func")
+    results_main.clear()
+    response = requests.get("https://www.midjourney.com/showcase/recent/")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    scripts = soup.find_all("script")
+    for script in scripts:
+        try:
+            bot.send_message(chat_id="652015662", text=f"Sending {script['id'][7:-2].lower()}...")
+            data = json.loads(script.text)
+            jobs = data['props']['pageProps']['jobs']
+            for job in jobs:
+                results_main.append({"link": job['image_paths'][0],
+                                     "prompt": job['full_command']
+                                     })
+
+        except KeyError:
+            continue
+
+main()
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
     # time.sleep(86400)
