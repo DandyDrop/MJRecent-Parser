2f6cb2ffdd1abc0263be066e7150e7ec85518814
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-01-27 08:08:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,19 +50,20 @@ def renew(m):
 
 @bot.message_handler(commands=["image"])
 def sendNewImage(m):
-    if len(main_dict[m.chat.id]) != 0:
-        try:
+    try:
+        if len(main_dict[m.chat.id]) != 0:
             image = main_dict[m.chat.id].pop()
             bot.send_photo(
                 chat_id=m.chat.id,
                 photo=image["link"],
                 caption=image["prompt"]
             )
-        except Exception as e:
+        else:
+            bot.send_message(chat_id=m.chat.id, text='You saw all images. Use "/renew" to see more or take a closer look to the images above ;) ')
+
+    except Exception as e:
             bot.send_message(chat_id="@logsmj", text=str(e))
             bot.send_message(chat_id=m.chat.id, text='Looks like I got no images for you. Try to "/renew"')
-    else:
-        bot.send_message(chat_id=m.chat.id, text='You saw all images. Use "/renew" to see more or take a closer look to the images above ;) ')
 
 
 def renew_main():
