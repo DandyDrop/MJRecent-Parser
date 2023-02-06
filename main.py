1c6d9e2115b12abe46319c0ba3c586ed689a519b
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-06 22:03:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import requests
 import telebot
 
+results_main = []
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 app = Flask(__name__)
 
@@ -24,11 +25,16 @@ app = Flask(__name__)
 @app.route('/', methods=['HEAD'])
 def handle_request():
     bot.send_message("@logsmj", "Detected HEAD request (adaptime)")
+    get_main()
+    return ""
+
+@app.route('/', methods=['POST'])
+def handle_request1():
+    bot.send_message("@logsmj", "Detected POST request (adaptime)")
     send_main()
     return ""
 
-  
-def send_main():
+def get_main():
     response = requests.get("https://www.midjourney.com/showcase/recent/")
     soup = BeautifulSoup(response.text, 'html.parser')
     scripts = soup.find_all("script")
@@ -37,16 +43,20 @@ def send_main():
             data = json.loads(script.text)
             jobs = data['props']['pageProps']['jobs']
             for job in jobs:
-                bot.send_photo(
-                    chat_id="@logsmj",
-                    photo=job['image_paths'][0],
-                    caption=job['full_command']
-                )
-                time.sleep(10)
+                results_main.append({"link": job['image_paths'][0],
+                                     "prompt": job['full_command']
+                                     })
 
             break
     
-
+def send_main():
+    if len(results_main) != 0:
+        image = results_main.pop()
+        bot.send_photo(
+            chat_id="@logsmj",
+            photo=image["link"],
+            caption=image["prompt"]
+        )
         
 def main():
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
