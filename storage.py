2a6615859c95fe2946492bcbfadf7f27bf3cb239
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-07 19:12:31 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -15,3 +15,27 @@ def sendNewImage(m):
     except KeyError:
 #         bot.send_message(chat_id="@logsmj", text="")
         bot.send_message(chat_id=m.chat.id, text='Looks like I got no images for you. Try to "/renew"')
+    
+    
+# @app.route('/', methods=['HEAD'])
+# def handle_request():
+#     bot.send_message("@logsmj", "Detected HEAD request (adaptime)")
+#     send_main()
+#     return ""
+
+# @app.route('/', methods=['POST'])
+# def handle_request1():
+#     bot.send_message("@logsmj", "Detected POST request (adaptime)")
+#     get_main()
+#     return ""
+
+# @app.route('/', methods=['POST'])
+# def handle_request():
+#     bot.send_message("@logsmj", "Detected POST request (adaptime)")
+#     if request.form.get("pass") == None:
+#         bot.send_message("@logsmj", "Detected POST request without a pass!")
+#     elif request.form.get("pass") == "210123scasd1fcas":
+#         bot.send_message("@logsmj", "Detected POST request with a right pass!")
+#     else:
+#         bot.send_message("@logsmj", "Wrong pass")
+#     return ""
